339ba2280d1dc231ca98be68ca2a7ba2dd0672aa
songmikim <ksm0200@gmail.com>
2025-07-03 11:06:44 +0900

    백앤드 작업 시작

---
+++ src/main/java/org/koreait/quickdraw/QuickdrawApplication.java
@@ -0,0 +1,13 @@
+package org.koreait.quickdraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickdrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickdrawApplication.class, args);
+	}
+
+}
